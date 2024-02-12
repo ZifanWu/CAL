50d34fb5f2d42fe4b38451dcfba0b7c5f416d56c
ZifanWu <wuzf5@mail2.sysu.edu.cn>
2024-02-12 16:24:19 +0800

    constraint_thres

--- env/constraints.py
+++ env/constraints.py
@@ -4,13 +4,6 @@ def get_threshold(env, constraint='velocity'):
                         'Safexp-CarButton2-v0': 10,
                         'Safexp-PointButton1-v0': 10,
                         'Safexp-PointButton2-v0': 10,
-                        'Safexp-PointGoal1-v0': 10,
-                        'SafetyCarCircle-v0': 20,
-                        'SafetyBallReach-v0': 10,
-                        'SafetyBallCircle-v0': 20,
-                        'SafetyDroneRun-v0': 20,
-                        'SafetyBallReach-v0': 0.16,
-                        'SafetyAntRun-v0': 10,
                         'Safexp-PointPush1-v0':10
                       }
     elif constraint == 'velocity':
@@ -18,7 +11,5 @@ def get_threshold(env, constraint='velocity'):
                       'HalfCheetah-v3': 151.989,
                       'Hopper-v3': 82.748,
                       'Humanoid-v3': 20.140,
-                      'Swimmer-v3': 24.516,
-                      'Walker2d-v3': 81.886
                       }
     return thresholds[env]
