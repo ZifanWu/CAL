f457bd80c3a71dc87d800153ccbac02f520bc5b3
ZifanWu <wuzf5@mail2.sysu.edu.cn>
2024-02-12 09:07:36 +0800

    remove_cache

--- arguments.py
+++ arguments.py
@@ -46,7 +46,7 @@ def readParser():
                         help='Policy Type: Gaussian | Deterministic (default: Gaussian)')
     parser.add_argument('--hidden_size', type=int, default=256)
     parser.add_argument('--lr', type=float, default=0.0003)
-    parser.add_argument('--qc_lr', type=float, default=0.0005)
+    parser.add_argument('--qc_lr', type=float, default=0.0003)
     parser.add_argument('--critic_target_update_frequency', type=int, default=2)
     parser.add_argument('--replay_size', type=int, default=1000000)
     parser.add_argument('--min_pool_size', type=int, default=1000) # default 1000
